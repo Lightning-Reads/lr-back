d23e3228fea0158314c73620056271e46fc1bda1
Sven Piller <sven.piller@dlh.de>
2017-10-28 23:07:50 +0200

    sort returned data bei importance and cut the detailed requests after an upperBound

--- workflow.py
+++ workflow.py
@@ -2,18 +2,25 @@
 
 from searchWikipedia import getWikipediaLink
 from searchGoogleimage import doImageSearch
+from importantWords import getWordCloud
+from operator import itemgetter
+
+upperBound = 5
 
 
 def generateContent(originContent):
 
+    count = 0
     mockData = {'Hackathon': 0.03434,
                 'SAP': 0.111,
                 'Jan Böhmermann': 0.001}
 
     originContent.decode("utf-8")
 
-    # wordlist = getWordlist(originContent)
-    wordlist = mockData
+    wordlist = getWordCloud(originContent)
+    # wordlist = mockData
+
+    wordlist = sorted(wordlist.iteritems(), key=itemgetter(1), reverse=True)
 
     result = {}
     result['baseText'] = originContent
@@ -21,16 +28,18 @@ def generateContent(originContent):
     result['helpfulLinks'] = []
     result['imageLinks'] = []
 
-    for key, value in wordlist.iteritems():
-        result['helpfulLinks'].append({
-            'word': key,
-            'url': getWikipediaLink(key)})
+    for key, value in wordlist:
         result['importantWords'].append({
             'word': key,
             'importance':  value})
-        result['imageLinks'].append({
-            'word': key,
-            'url': getWikipediaLink(key)})
+        if count < upperBound:
+            result['helpfulLinks'].append({
+                'word': key,
+                'url': getWikipediaLink(key)})
+            result['imageLinks'].append({
+                'word': key,
+                'url': doImageSearch(key)})
+            count = count + 1
 
     return result
 
